6a07e96a4f578a09f302c4aee6f557497a8be5da
mitchanx <kommital@yandex.ru>
2026-06-22 00:28:29 +0300

    ready for remote-rebuilding + mounts + update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -2,13 +2,24 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+    [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
+
+  fileSystems."/mnt/hdd" = { 
+    device = "/dev/disk/by-uuid/2eeedad6-fb5e-434e-a6a0-f340f7503eb1";
+    fsType = "ext4";
+    options = [ "defaults" "noatime" "users" "nofail" "exec" ];
+    neededForBoot = false;
+  };
+
+  swapDevices = [ {
+    device = "/.swapfile";
+    size = 4 * 1024;
+  } ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
